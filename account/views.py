04a2181fc27e7b209480dcc7b785b268d6007b28
unknown <hiahmadyan@gmail.com>
2022-06-14 19:07:20 +0430

    adding account app and SignUpView and it's url

---
+++ account/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import FormView
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SignUpView(FormView):
+    form_class = UserCreationForm
+    template_name = 'signup.html'
