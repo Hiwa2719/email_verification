99097cca428411a945c38c7ec318f77daf7d8402
unknown <hiahmadyan@gmail.com>
2022-06-14 19:56:33 +0430

    adding SignUpFrom

---
+++ account/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()
+
+
+class SignUpFrom(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = 'password1', 'password2'
+
+    def order_fields(self, field_order):
+        return super().order_fields(['email', 'password1', 'password2'])
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        user = User.objects.filter(email=email)
+        if user.exists():
+            raise forms.ValidationError('User with this email already exists')
+        return email
+
+    def save(self, commit=True):
+        email = username = self.cleaned_data.get('email')
+        user = User(username=username, email=email)
+        user.set_password(self.cleaned_data.get('password1'))
+        user.save()
+        return user
--- account/views.py
+++ account/views.py
@@ -1,7 +1,14 @@
+from django.urls import reverse_lazy
 from django.views.generic import FormView
-from django.contrib.auth.forms import UserCreationForm
+
+from .forms import SignUpFrom
 
 
 class SignUpView(FormView):
-    form_class = UserCreationForm
+    form_class = SignUpFrom
     template_name = 'account/signup.html'
+    success_url = reverse_lazy('account:signup')
+
+    def form_valid(self, form):
+        form.save()
+        return super(SignUpView, self).form_valid(form)
