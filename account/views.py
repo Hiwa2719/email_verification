a4d37200a552f98e14e136b94c2263fbeaad54f1
unknown <hiahmadyan@gmail.com>
2022-06-15 13:17:43 +0430

    adding email template

--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
+from django.core.mail import send_mail
 from django.urls import reverse_lazy
-from django.views.generic import FormView
-
+from django.views.generic import FormView, View
+from django.template.loader import get_template
+from django.contrib.auth.tokens import
 from .forms import SignUpFrom
 
 
@@ -10,5 +12,17 @@ class SignUpView(FormView):
     success_url = reverse_lazy('account:signup')
 
     def form_valid(self, form):
-        form.save()
+        user = form.save()
+        self.send_email(user)
         return super(SignUpView, self).form_valid(form)
+
+    def send_email(self, user):
+        template = get_template('account/email_template.html')
+
+        send_mail(subject='Email Verification', message='Your Email verification Link',
+                  recipient_list=[user.email], html_message='')
+
+
+class GenerateEmail(View):
+    def get(self, request, *args, **kwargs):
+        pass
